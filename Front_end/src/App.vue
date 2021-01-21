eda10cc14bea413896e6c0e83c3f46e4c820ba02
Eslam Darwish <islam.darwish.hassan@gmail.com>
2021-01-21 15:13:01 +0200

    first audition of prescription page

--- Front_end/src/App.vue
+++ Front_end/src/App.vue
@@ -12,14 +12,14 @@
 import { IonApp, IonRouterOutlet } from '@ionic/vue';
 import { defineComponent } from 'vue';
 // import ProgressBar from './Components/ProgressBar.vue';
-import Signup from './Signup.vue';
+// import Signup from './Signup.vue';
 export default defineComponent({
   name: 'App',
   components: {
     IonApp,
     IonRouterOutlet,
     // ProgressBar,
-    Signup
+    // Signup
     
   }
 });
