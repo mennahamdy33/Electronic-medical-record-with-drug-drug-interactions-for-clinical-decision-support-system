ad9cc4f9c61097bf8ba0a823f260fd1afee23604
nancy salah <nancysalah97@hotmail.com>
2021-01-28 18:17:09 +0200

    edited the prescription page

--- Front_end/src/views/pages/Prescription.vue
+++ Front_end/src/views/pages/Prescription.vue
@@ -1,125 +1,110 @@
 <template>
-  <ion-card>
-    <ion-card-header>
-      <ion-avatar>
-        <img
-          src="https://lh3.googleusercontent.com/proxy/btl-O2nMnVcynDfOi-sVQgcrbxbfw2UC3Jgsa2Hg5-VFT69A8XuCALolPVDhLeQI1YwvzlnSoiiA96UnNzZzH6DXoQ06CiKRgsMt5ldtXTSa-YFocM7vyEs"
-        />
-      </ion-avatar>
-      <ion-card-subtitle>Dentist</ion-card-subtitle>
-      <ion-card-title> Dr. Nancy </ion-card-title>
-    </ion-card-header>
-
-    <ion-card-content> 12 street dr.nancy,cairo,egypt </ion-card-content>
-  </ion-card>
-
-  <!-- Input fields go here -->
-  <ion-card>
+  <ion-header>
+    <ion-toolbar color="light">
+      <ion-buttons slot="start">
+        <ion-menu-button></ion-menu-button>
+      </ion-buttons>
+      <ion-title>Prescription</ion-title>
+    </ion-toolbar>
+  </ion-header>
+  <ion-content>
     <ion-grid>
       <ion-row>
+        <ion-col></ion-col>
         <ion-col>
-          <ion-item>
-            <ion-label position="floating">Name</ion-label>
-            <ion-input></ion-input>
-          </ion-item>
-          <ion-item style="margin-top: 10px">
-            <ion-label position="floating">Address</ion-label>
-            <ion-input></ion-input>
-          </ion-item>
+          <ion-card>
+            <ion-card-header>
+              <ion-avatar>
+                <img v-bind:src="patientInfo.patientPhoto" />
+              </ion-avatar>
+              <ion-card-subtitle>{{
+                patientInfo.patientGender
+              }}</ion-card-subtitle>
+              <ion-card-title> {{ patientInfo.patientName }} </ion-card-title>
+            </ion-card-header>
+            <ion-card-content>
+              <p>Age : {{ patientInfo.patientAge }}</p>
+              <p>Mobile : {{ patientInfo.patientPhone }}</p>
+            </ion-card-content>
+          </ion-card>
+
+          <ion-card>
+            <ion-card-header>
+              <ion-card-subtitle>Diagnosis</ion-card-subtitle>
+            </ion-card-header>
+            <ion-card-content>
+              <ion-textarea
+                placeholder="Enter more information here..."
+              ></ion-textarea>
+            </ion-card-content>
+          </ion-card>
+
+          <ion-card>
+            <ion-card-header>
+              <ion-card-subtitle>Drugs List</ion-card-subtitle>
+            </ion-card-header>
+            <ion-card-content>
+              <ion-searchbar show-cancel-button="never"></ion-searchbar>
+
+              <ion-list>
+                <ion-item
+                  style="d-flex justify-items-between"
+                  :key="item.id"
+                  v-for="item in drugsInfo"
+                >
+                  <ion-label> {{ item.name }}</ion-label>
+                  <ion-button color="danger">Delete</ion-button>
+                </ion-item>
+              </ion-list>
+            </ion-card-content>
+          </ion-card>
         </ion-col>
-        <ion-col>
-          <ion-item>
-            <ion-label position="floating">DOB</ion-label>
-            <ion-datetime
-              display-format="MM DD YY"
-              placeholder="Select Date"
-            ></ion-datetime>
-          </ion-item>
-          <ion-item style="margin-top: 10px">
-            <ion-label position="floating">Date</ion-label>
-            <ion-datetime
-              display-format="MM DD YY"
-              placeholder="Select Date"
-            ></ion-datetime>
-          </ion-item>
-        </ion-col>
+        <ion-col></ion-col>
       </ion-row>
     </ion-grid>
-  </ion-card>
-
-  <!-- List of Text Items -->
-  <ion-grid>
-    <ion-row>
-      <ion-col>
-        <ion-text>
-          <h2>Disp</h2>
-        </ion-text>
-        <ion-button
-          name="remove-circle-outline"
-          @click="decrement"
-        ></ion-button>
-        1
-        <ion-button name="add-circle" @click="increment"></ion-button>
-        <ion-text>
-          <h2>Tabs</h2>
-        </ion-text>
-        <ion-text>
-          <h2>Refill</h2>
-        </ion-text>
-        <ion-button name="remove-circle" @click="decrement"></ion-button>
-        1
-        <ion-button name="add-circle" @click="increment"></ion-button>
-        <ion-text>
-          <h2>Tabs</h2>
-        </ion-text>
-      </ion-col>
-      <ion-col size="3">
-        <ion-card>
-          <ion-card-header>
-            <ion-card-subtitle>Drugs List</ion-card-subtitle>
-          </ion-card-header>
-          <ion-card-content>
-            <ion-searchbar show-cancel-button="never"></ion-searchbar>
-
-            <ion-list>
-              <ion-item style="d-flex justify-items-between">
-                <ion-label>Drug 1</ion-label>
-                <ion-button color="danger">Delete</ion-button>
-              </ion-item>
-              <ion-item>
-                <ion-label>Drug 2</ion-label>
-              </ion-item>
-              <ion-item>
-                <ion-label>Drug 3</ion-label>
-              </ion-item>
-            </ion-list>
-          </ion-card-content>
-        </ion-card>
-        <ion-card>
-          <ion-item>
-            <ion-label text-align="center" position="floating"
-              >Doctor Signature</ion-label
-            >
-            <ion-input></ion-input>
-          </ion-item>
-        </ion-card>
-      </ion-col>
-    </ion-row>
-  </ion-grid>
+    <ion-footer>
+      <ion-item>
+        <ion-label position="fixed">Doctor Signature</ion-label>
+        <ion-input></ion-input>
+      </ion-item>
+    </ion-footer>
+  </ion-content>
 </template>
-
 <script>
 import { IonInput } from "@ionic/vue";
 import { defineComponent } from "vue";
+import axios from "axios";
 
 export default defineComponent({
   name: "nancy",
   components: {
     IonInput,
   },
-
+  data() {
+    return { patientInfo: { patientPhoto: "" }, drugsInfo: [] };
+  },
+  async mounted() {
+    //try to get users data
+    await this.get_user();
+    //try to get drugs data
+    await this.get_drugs();
+  },
   setup() {},
   methods: {
+    async get_user() {
+      axios.get("http://localhost:3000/users/1").then((response) => {
+        this.patientInfo = response.data;
+        //console
+        console.log(response.data);
+      });
+    },
+    async get_drugs() {
+      axios.get("http://localhost:3000/drugs").then((response) => {
+        this.drugsInfo = response.data;
+        //console
+        console.log(response.data);
+      });
+    },
     decrement() {
       console.log("decrement");
     },
@@ -129,3 +114,4 @@ export default defineComponent({
   },
 });
 </script>
+<style scoped></style>
