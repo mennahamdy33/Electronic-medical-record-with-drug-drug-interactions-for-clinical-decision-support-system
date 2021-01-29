8342f9ba1cab7eb6808a8087dd3444427a9fe15b
norantharowat <norantharowat@gmail.com>
2021-01-29 21:57:49 +0200

    adding server folder

---
+++ Server/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+// const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const Sequelize = require('sequelize');
+
+const app = express();
+
+app.use(cors())
+app.use(bodyParser.json());
+
+const db = new Sequelize('Drug_Data', 'root', 'mysql', {
+    host: 'localhost',
+    dialect: 'mysql' 
+});
+
+db.authenticate()
+.then(() => {
+    console.log('Connection has been established successfully.');
+})
+.catch(err => {
+    console.error('Unable to connect to the database:', err);
+});
+app.listen(3000, ()=> {
+    console.log('app is running on port 3000');
+  })
