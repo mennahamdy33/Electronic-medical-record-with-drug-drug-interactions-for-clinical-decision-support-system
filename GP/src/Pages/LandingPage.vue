2895e92783a6e4c2623aabce282a1a34100e5099
norantharowat <norantharowat@gmail.com>
2021-02-01 22:15:23 +0200

    adding doctor and secretary cards

--- GP/src/Pages/LandingPage.vue
+++ GP/src/Pages/LandingPage.vue
@@ -4,13 +4,47 @@
   <ion-content class="content">
 
     <ion-grid>
-      <ion-row>
-        <ion-col size-lg="2">
-          <form-button router-link="/Signup" type="button" buttonText="Signup"/>
+      <ion-row style="margin-top: 2%" class="ion-justify-content-center">
+        <ion-col >
+          <h2> Sign up </h2>
+          <!-- <form-button router-link="/Signup" type="button" buttonText="Signup"/> -->
+        </ion-col>
+      </ion-row>
+      <ion-row style="margin-top: 2%" class="ion-justify-content-center">
+        <ion-col size-lg="3" size-xs="11" >
+          <ion-card router-link="/Signup" button=true>
+            <img src="../../public/assets/Untitled-5.svg"  > 
+             <ion-card-header>
+              <!-- <ion-card-subtitle>Card Subtitle</ion-card-subtitle> -->
+              <ion-card-title> As a Doctor</ion-card-title>
+            </ion-card-header>
+
+            <!-- <ion-card-content>
+              Keep close to Nature's heart... and break clear away, once in awhile,
+              and climb a mountain or spend a week in the woods. Wash your spirit clean.
+            </ion-card-content> -->
+          </ion-card>
+        </ion-col>
+
+        <ion-col offset-lg="1" size-lg="3" size-xs="11">
+          <ion-card>
+            <img src="../../public/assets/Untitled-6.svg"  > 
+             <ion-card-header>
+              <!-- <ion-card-subtitle>Card Subtitle</ion-card-subtitle> -->
+              <ion-card-title>As a Secretary</ion-card-title>
+            </ion-card-header>
+
+            <!-- <ion-card-content>
+              Keep close to Nature's heart... and break clear away, once in awhile,
+              and climb a mountain or spend a week in the woods. Wash your spirit clean.
+            </ion-card-content> -->
+          </ion-card>
         </ion-col>
       </ion-row>
       <!-- <router-link to="/MakeAccount"><form-button type="button" buttonText="Signup"/></router-link> -->
     </ion-grid>
+
+    
   </ion-content>
 </ion-page>
 <!-- </body> -->
@@ -18,20 +52,23 @@
 
 <script>
 import { defineComponent } from 'vue';
-import { IonContent , IonPage, IonGrid , IonCol, IonRow  } from '@ionic/vue';
-import FormButton from '../Components/FormButton.vue';
+import { IonContent , IonPage, IonGrid , IonCol, IonRow,IonCard ,IonCardTitle,IonCardHeader } from '@ionic/vue';
+// import FormButton from '../Components/FormButton.vue';
 // import FormField from '../Components/FormField'
 // import ProgressBar from '../Components/ProgressBar.vue';
 export default defineComponent({
   name: 'LandingPage',
   components: {
-    FormButton,
+    // FormButton,
     // FormField,
     IonCol,
     IonGrid,
     IonRow,
     IonContent,
-    IonPage 
+    IonPage ,
+    IonCard,
+    IonCardTitle,
+    IonCardHeader
     // ProgressBar,
     
     
@@ -64,6 +101,9 @@ export default defineComponent({
   /* background: linear-gradient(#c4c5c5a2, #8a8989); */
   /* background: linear-gradient( #54af94c0,#aaf8e1c0); */
   --background: linear-gradient(#141e30, #243b55);
+  /* --background: linear-gradient( #141e30,#28425f, #294361,#141e30); */
+  /* --background: linear-gradient( #336450,#398666, #3a8a69,#336450); */
+  /* --background: linear-gradient( #214436,#336450, #286d50,#336450); */
   
 }
 .FormGrid ion-col{
@@ -71,59 +111,29 @@ export default defineComponent({
   /* --ion-grid-padding: 50px; */
 }
 
-/* .login-box { */
-  
-  /* position: absolute; */
-  /* top: 55%; */
-  /* left: 35%; */
-  /* display: block;
-  margin-left: auto;
-  margin-right: auto; */
 
-  /* width: 50%; */
-  /* width: 500px; */
-  /* padding: 40px; */
-  /* transform: translate(-50%, -50%); */
-  /* background: rgba(31, 29, 29, 0.281); */
-  /* box-sizing: border-box; */
-  /* box-shadow: 0 15px 25px rgba(0,0,0,.6); */
-  /* border-radius: 10px; */
-/* } */
 
-.login-box h2 {
+h2 {
   margin: 0 0 30px;
   padding: 0;
   color: #ffffff;
-  /* color: #05545a; */
+  
+  font-family: "Lucida Console", "Lucida Sans Typewriter", monaco, "Bitstream Vera Sans Mono", monospace; 
   text-align: center;
   font-weight: bold;
 }
 
-/* .image{
+ion-card-title{
   text-align: center;
   
 
+}
+
+
+/* ion-card{
+  height: 500px;
 } */
 
-
-/* .image img{ */
-  /* width: 20%; */
-  /* display: block; */
-  /* margin-left: auto; */
-  /* margin-right: auto; */
-  /* margin-bottom: 50px; */
-  
-  /* width: 40%; */
-  
-  
-/* } */
-
-/* form{
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-gap: 20px;
-  
-} */
 
 
 </style>
