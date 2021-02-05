bdda85a18b95ad9012f698ad0914ee062bc1c082
Mennatallah Hamdy <menna.h.m.elsayed@gmail.com>
2021-02-05 14:11:35 +0200

    bb

--- GP/src/router/index.js
+++ GP/src/router/index.js
@@ -1,23 +1,44 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
-// import Signup2 from '../Pages/Signup2.vue'
+import AddPatientPage from '../Pages/DSProfiles/AddPatientPage.vue'
 import Signup from '../Pages/Signup/Signup.vue'
 
 const routes = [
-  {
-    path: '/',
-    redirect: '/home'
-  },
-  {path: '/Signup' , component: Signup},
-  // {path: '/MakeAccount' , component: Signup2}
-  
-]
+    {
+        path: '/',
+        redirect: '/home'
+    },
+    {path: '/Signup' , component: Signup},
+    {
+        path : '/addpatient',
+        component: AddPatientPage
+    },
+    {
+        path:'/patientProfile',
+        component:() => import('../Pages/DSProfiles/PatientProfilePage')
+    },
+    {
+        path:'/doctorProfile',
+        component:() => import('../Pages/DSProfiles/DoctorProfilePage')
+    },
+
+];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
 })
 
 export default router
+
+
+
+
+
+
+
+
+
+
 
 
 
