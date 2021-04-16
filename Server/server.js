f4ae5b7d118f2ba9a2053ad6a00c49473cd38ba3
norantharowat <norantharowat@gmail.com>
2021-04-16 17:49:05 +0200

    login

--- Server/server.js
+++ Server/server.js
@@ -122,6 +122,52 @@ app.post('/register', async (req, res) => {
 
 })
 
+app.post('/signin', (req, res) => {
+  const {email, password,proficiency} = req.body;
+
+  if(proficiency === 'doctor'){
+    db.select('email', 'password').from('doctors')
+    .where('email', '=', email)
+    .then(async (data) => {
+      const isValid = await bcrypt.compareSync(password, data[0].password);
+      if (isValid) {
+        return db.select( 'doctor_id', 'first_name', 'last_name').from('doctors')
+          .where('email', '=', email)
+          .then(user => {
+            console.log(user[0])
+            res.json(user[0])
+          })
+          .catch(err => res.status(400).json('unable to get user'))
+      } else {
+        res.status(400).json('wrong credentials')
+      }
+    })
+    .catch(err => res.status(400).json('wrong credentials'))
+  }
+  else if(proficiency === 'secretary'){
+    db.select('email', 'password').from('secretary')
+    .where('email', '=', email)
+    .then(async (data) => {
+      const isValid = await bcrypt.compareSync(password, data[0].password);
+      if (isValid) {
+        return db.select( 'secretary_id', 'first_name', 'last_name').from('secretary')
+          .where('email', '=', email)
+          .then(user => {
+            console.log(user[0])
+            res.json(user[0])
+          })
+          .catch(err => res.status(400).json('unable to get user'))
+      } else {
+        res.status(400).json('wrong credentials')
+      }
+    })
+    .catch(err => res.status(400).json('wrong credentials'))
+  }
+
+ 
+})
+
+
 app.get('/clinics/:id', (req, res) => {
 
   const  id  = req.params.id;
