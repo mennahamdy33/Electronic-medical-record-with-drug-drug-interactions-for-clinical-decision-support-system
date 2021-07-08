4c0e502bb97d48e52b58888fd01d36e937b16e56
norantharowat <norantharowat@gmail.com>
2021-07-08 10:34:34 +0200

    presc requests

--- GP/src/Pages/Purchase/Login.vue
+++ GP/src/Pages/Purchase/Login.vue
@@ -11,14 +11,14 @@
                         <!-- <img src="../../../public/assets/logo4.png"  >    -->
                         </div>
                     </ion-col>
-                    <ion-col size-lg="3" offset-lg="0.9">
+                    <ion-col size-lg="3" offset-lg="0.9" size-xs="6" offset-md="0" >
 
                                         
                         <ion-button   class="signup" router-link="/SignupPurchase"> Sign up</ion-button>
               
                     </ion-col>
 
-                     <ion-col pull-lg="1.5"  pull-xs="0"  size-lg="3" size-xs="15"  >
+                     <ion-col pull-lg="1.5"  pull-xs="0"  size-lg="3" size-xs="6" pull-md="3" >
                                         
                         <ion-button router-link="/LoginPurchase" class="login" > Log in</ion-button>
      
